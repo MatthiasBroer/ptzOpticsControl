2146487800df6ebe782c50d8378bbfd8c64ad765
Matthias <matthiasbroer@outlook.com>
2021-10-25 20:00:26 +0200

    // [change] default layout
    The default layout is ready. No functions yet

---
+++ controller.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html>
+    <head>
+        <title>
+            PTZ Optics Controller
+        </title>
+    </head>
+    <body>
+        <h1 style="font-size: 5vw">
+            Welkom op bij PTZ Optics Controller
+            <img src="afbeeldingen/logo/MBEngineringLogoV2.png" alt="MB engineering" width=20%>
+        </h1>
+        <p style="font-size: 3vw">
+            Op deze pagina kun je je jouw ptz optic cameras bedienen <br> met simple drukken op de knop
+        </p>
+        <p>
+            <a href="index.html"> HOME</a> 
+        </p>
+        <p>
+            <form method="post">
+                <table border='0'>
+                    <tr>
+                        <td>
+                        <td>
+                        <td>
+                            <input name="omhoog" type="submit" value="omhoog" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <input name="links" type="submit" value="links" />
+                        </td>
+                        <td>
+                        <td>
+                        <td>
+                            <input name="rechts" type="submit" value="rechts" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                        <td>
+                        <td>
+                            <input name="omlaag" type="submit" value="omlaag" />
+                        </td>
+                    </tr>
+                </table>    
+            </form>
+        </p>
+    </body>
+</html>
