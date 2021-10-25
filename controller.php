dc3a32b04e6f1c8bc3f5328a151471d5e250cd6b
Matthias <matthiasbroer@outlook.com>
2021-10-25 21:47:47 +0200

    //[change] file controller.php
    some changes toe controller.php. Removed camera control code from file and moved to controllerPagina.php

--- controller.php
+++ controller.php
@@ -8,7 +8,7 @@
     <body>
         <h1>
             Welkom op bij PTZ Optics Controller <br>
-            <img src="afbeeldingen/logo/MBEngineringLogoV2.png" alt="MB engineering" width=20%>
+            <img src="afbeeldingen/logo/MBEngineringLogoV2.png" alt="MB engineering" width=100px>
         </h1>
         <p>
             Op deze pagina kun je je jouw ptz optic cameras bedienen <br> met simple drukken op de knop
@@ -17,109 +17,33 @@
             <a href="index.html"> HOME</a> 
         </p>
         <pack>
-            <form method="post">
+            <form action="controllerPagina.php"method="post">
                 <table border='0'>
                     <tr>
                         <td>
                             <strong>Camera IP adres: <strong>
                         </td>
                         <td>
-                            <input type="text" placeholder="192.168.2.xxx" name="ipCam" size="8" minlength="13" maxlength="13" required>
-                        </td>
-                    </tr>
-                </tbale>
-                <table border='0'>
-                    <tr>
-                        <td>
-                        <td>
-                        <td>
-                            <input name="omhoog" type="submit" value="omhoog" />
+                            <input type="text" placeholder="192.168.2.xxx" name="ipCam" size="8" minlength="13" maxlength="13">
                         </td>
                         <td>
-                        </td>
-                        <td>
-                            <input name="zoomIn" type="submit" value="+" />
-                        </td>
+                            <input name="setIp" type="submit" value="zet ip adres vast" />
                     </tr>
                     <tr>
-                        <td>
-                            <input name="links" type="submit" value="links" />
+                        <td> 
+                            <strong>Huidige IP adres: <strong>
                         </td>
                         <td>
-                        <td>
-                        <td>
-                            <input name="rechts" type="submit" value="rechts" />
-                        </td>
-                        <td>
-                            <input name="zoomStop" type="submit" value="stop" />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                        <td>
-                        <td>
-                            <input name="omlaag" type="submit" value="omlaag" />
-                        </td>
-                        <td>
-                        </td>
-                        <td>
-                            <input name="zoomUit" type="submit" value="-" />
+                            <?php 
+                                $file = 'ip.txt';
+                                $current = file_get_contents($file);
+                                echo $current;
+                            ?>
                         </td>
                     </tr>
                 </table>    
             </form>
-            <?php
-                error_reporting(0);
-                if (isset($_POST['omhoog']))
-                {
-                    $camUrl = $_POST['ipCam']; // get the ip form user input
-                    $url = $camUrl . '/cgi-bin/ptzctrl.cgi?ptzcmd&'; // url for cam
-                    $action = 'UP&1&10'; // the actiond for the camera to do
-                    $requestUrl = $url. $action;
-                }
-                if (isset($_POST['links']))
-                {
-                    $camUrl = $_POST['ipCam']; // get the ip form user input
-                    $url = $camUrl . '/cgi-bin/ptzctrl.cgi?ptzcmd&'; // url for cam
-                    $action = 'LEFT&10&1'; // the actiond for the camera to do
-                    $requestUrl = $url. $action;
-                }
-                if (isset($_POST['rechts']))
-                {
-                    $camUrl = $_POST['ipCam']; // get the ip form user input
-                    $url = $camUrl . '/cgi-bin/ptzctrl.cgi?ptzcmd&'; // url for cam
-                    $action = 'RIGHT&10&1'; // the actiond for the camera to do
-                    $requestUrl = $url. $action;
-                }
-                if (isset($_POST['omlaag']))
-                {
-                    $camUrl = $_POST['ipCam']; // get the ip form user input
-                    $url = $camUrl . '/cgi-bin/ptzctrl.cgi?ptzcmd&'; // url for cam
-                    $action = 'DOWN&1&10'; // the actiond for the camera to do
-                    $requestUrl = $url. $action;
-                }
-                if (isset($_POST['zoomIn']))
-                {
-                    $camUrl = $_POST['ipCam']; // get the ip form user input
-                    $url = $camUrl . '/cgi-bin/ptzctrl.cgi?ptzcmd&'; // url for cam
-                    $action = 'ZOOMIN&5'; // the actiond for the camera to do
-                    $requestUrl = $url. $action;
-                }
-                if (isset($_POST['zoomUit']))
-                {
-                    $camUrl = $_POST['ipCam']; // get the ip form user input
-                    $url = $camUrl . '/cgi-bin/ptzctrl.cgi?ptzcmd&'; // url for cam
-                    $action = 'ZOOMOUT&5'; // the actiond for the camera to do
-                    $requestUrl = $url. $action;
-                }
-                if (isset($_POST['zoomStop']))
-                {
-                    $camUrl = $_POST['ipCam']; // get the ip form user input
-                    $url = $camUrl . '/cgi-bin/ptzctrl.cgi?ptzcmd&'; // url for cam
-                    $action = 'ZOOMSTOP&5'; // the actiond for the camera to do
-                    $requestUrl = $url. $action;
-                }
-            ?> 
+            
         </p>
         <footer class="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-37b0"><div class="u-clearfix u-sheet u-sheet-1">
             <p class="u-small-text u-text u-text-variant u-text-1">MB engineering©2021 All rights reserved</p>
